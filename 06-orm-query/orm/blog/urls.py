b92016f3c262975037b08c60bc7344c0c2b44248
Raj Bhatta <bhattaraj21@gmail.com>
2022-04-04 21:36:31 -0700

    M1: Working with query in Django

---
+++ 06-orm-query/orm/blog/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('', view, function_name)
+    path('', views.home, name='blog-home'),
+
+]
